61cd51661286bb954f002d73e7d98b0b211cd432
LeslieAine <aineleslie@gmail.com>
2023-01-30 15:38:05 +0300

    fix linter errors

--- src/task-class.js
+++ src/task-class.js
@@ -1,9 +1,9 @@
 class Task {
-    constructor(index, desc, completed) {
-      this.index = index;
-      this.desc = desc;
-      this.completed = completed;
-    }
+  constructor(index, desc, completed) {
+    this.index = index;
+    this.desc = desc;
+    this.completed = completed;
   }
-  
-  export default Task;
+}
+
+export default Task;
